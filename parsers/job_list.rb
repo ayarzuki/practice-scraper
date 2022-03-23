1200c1a3e6d546b507c7117500f8d5042a0f44a4
ayarzuki <aulya.yarzuki@gmail.com>
2022-03-23 20:13:08 +0700

    updating job_detail to create industry map and updating job_list script to load pagination

--- parsers/job_list.rb
+++ parsers/job_list.rb
@@ -17,13 +17,29 @@ positions.each do |position|
     }
 end
 
-# next_page = html.at_css('ul.pager li.next a')
-# if next_page
-# 	url_next = "https://www.manpower.ca#{URI.encode(next_page["href"])}"
-# 	pages << 	{
-# 		page_type: "job_list",
-# 		url: url_next,
-# 		vars: {
-# 		},
-# 	}
+# next_page = html.at_css('ul.pager li.hidden-xs a')
+# next_page.each do |link|
+#     pagination = link['onclick="listPagination(#{i})"']
+#     if pagination =~ /[0-9]/
+#         page_next = "https://www.manpower.ca/ManpowerCanada/search-jobs?txtJobType=&txtLocation=&txtLatitude=&txtLongitude=&source="
+#         pages << 	{
+#             page_type: "job_list",
+#             # url: url_next,
+#             # vars: {
+#             # },
+#         }
+# end
+
+# next_page = html.at_css('ul.pager li.hidden-xs a')
+# 100.times do |i|
+#     url = "https://www.manpower.ca/ManpowerCanada/search-jobs?txtJobType=&txtLocation=&txtLatitude=&txtLongitude=&source="
+#     pagination = next_page['listPagination("#{i}")']
+#     pages << {
+#         page_type: "job_list",
+#         url: url,
+#         pagination: pagination,
+#         # vars: {
+#         #     page_number: vars["page_number"] + 1
+#         # },
+#     }
 # end
