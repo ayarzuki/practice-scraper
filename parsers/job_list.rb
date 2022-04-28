7f0a268ad0ce2911e8452c5e0bc92a76366b0bb8
ayarzuki <aulya.yarzuki@gmail.com>
2022-04-28 22:57:53 +0700

    first commit for initialization project

--- parsers/job_list.rb
+++ parsers/job_list.rb
@@ -1,3 +1,11 @@
+require "json"
+
+# This time we parse the JSON response
+json = JSON.parse(content)
+rank = 1
+page_num = page["vars"]["page_num"]
+
+
 html = Nokogiri.HTML(content)
 vars = page['vars']
 
@@ -28,13 +36,6 @@ end
 
 # you can try to fetch the pagination page one by one
 page_number = vars['page_number'] || 1
-# p page_number
-# base_url = html.at("base").attr("href")
-# p base_url
-# resource_pagination_url = content.split('var resourcepaginationURL = "', 2).last.split('";').first
-# p resource_pagination_url
-
-# p (base_url + resource_pagination_url)
 
 pages << {
   # url: "#{base_url}#{resource_pagination_url}",
@@ -70,45 +71,3 @@ pages << {
   # no_url_encode: true,
   # http2: true,
 }
-
-# or all at once
-# if page_number == 1
-#   total_count = html.at(".row.search-result-head").text.scan(/\d+/)
-#   max_page = (Float(total_count) / Float(20)).floor
-
-#   (2..max_page).each do |next_page_number|
-#     pages << {
-#       url: "#{base_url}#{resource_pagination_url}",
-#       page_type: 'job_list',
-#       method: 'POST',
-#       headers: {
-#         'Accept' => '*/*',
-#         'Accept-Encoding' => 'gzip, deflate, br',
-#         'Accept-Language' => 'en-US,en;q=0.5',
-#         'Connection' => 'keep-alive',
-#         'Content-Type' => 'application/x-www-form-urlencoded; charset=UTF-8',
-#         'Cookie' => 'UqZBpD3n3iPIDwJU=v1b1lJg8acb60; s_cc=true; s_nr=1648042288600; s_vnum=1650634222400%26vn%3D1; s_invisit=true; s_sq=mpdtprodcandidate%3D%2526pid%253DJob%252520Search%2526pidt%253D1%2526oid%253Dfunctionanonymous%252528e%252529%25257Bvars%25253Ds_c_il%25255B0%25255D%25252Cb%25253Ds.eh%252528this%25252C%252522onclick%252522%252529%25253Bs.lnk%25253Ds.co%252528this%252529%25253Bs.t%252528%252529%25253Bs.lnk%25253D0%25253Bif%252528b%252529retur%2526oidt%253D2%2526ot%253DA; G_ENABLED_IDPS=google; JSESSIONID=0000aVR6SEnoW8Zd8CoGA15Madg:1dotsh9v6; __hstc=32730849.50e2cc91b18a3177d3c6de59dd9b8866.1648042235600.1648042235600.1648042235600.1; hubspotutk=50e2cc91b18a3177d3c6de59dd9b8866; __hssrc=1; __hssc=32730849.2.1648042235600',
-#         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
-#         'X-Requested-With' => 'XMLHttpRequest'
-#       },
-#       body: URI.encode_www_form({'x' => next_page_number}),
-#       vars: {
-#         page_number: next_page_number
-#       }
-#     }
-#   end
-# end
-
-# next_page = html.at_css('ul.pager li.hidden-xs a')
-# 100.times do |i|
-#     url = "https://www.manpower.ca/ManpowerCanada/search-jobs?txtJobType=&txtLocation=&txtLatitude=&txtLongitude=&source="
-#     pagination = next_page['listPagination("#{i}")']
-#     pages << {
-#         page_type: "job_list",
-#         url: url,
-#         pagination: pagination,
-#         # vars: {
-#         #     page_number: vars["page_number"] + 1
-#         # },
-#     }
-# end
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,32 +1,37 @@
+body = {
+    "txtJobType:" => "",
+    "txtLocation:" => "",
+    "txtLatitude:" => "",
+    "txtLongitude:" => "",
+    "source:" => "",
+}
+
 pages << {
     page_type: 'job_list',
     method: "GET",
     force_fetch: true,
-    # fetch_type: "browser",
+    fetch_type: "browser",
     headers: {
-        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        ":authority" => "www.manpower.ca",
+        ":method" => "GET",
+        ":path" => "/ManpowerCanada/search-jobs?txtJobType=&txtLocation=&txtLatitude=&txtLongitude=&source=",
+        "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-encoding" => "gzip, deflate, br",
-        "User-Agent" => "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0",
-        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+        "cookie" => "G_ENABLED_IDPS=google; hubspotutk=cfb4162b3aeaf6be0439ff8164d9474b; _ga=GA1.2.484422288.1648474523; UqZBpD3n3iPIDwJU=v1bFlJg8achqM; s_cc=true; s_nr=1651160038894; s_vnum=1653752038895&vn=1; s_invisit=true; s_sq=[[B]]; __hstc=32730849.cfb4162b3aeaf6be0439ff8164d9474b.1647879910303.1649771875857.1651160042618.42; __hssrc=1; __hssc=32730849.1.1651160042618",
+        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+        "sec-ch-ua" => '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
         "sec-ch-ua-mobile" => "?0",
-        "sec-ch-ua-full-version-list" => 'Not A;Brand";v="99.0.0.0", "Chromium";v="99.0.4844.51", "Google Chrome";v="99.0.4844.51"',
-        "sec-ch-ua-platform" => '"Linux"',
+        "sec-ch-ua-platform" => '"Windows"',
         "sec-fetch-dest" => "document",
         "sec-fetch-mode" => "navigate",
         "sec-fetch-site" => "cross-site",
-        "upgrade-insecure-requests" => "1",
-        "content-type" => "text/html; charset=UTF-8",
     },
     url: "https://www.manpower.ca/ManpowerCanada/search-jobs?txtJobType=&txtLocation=&txtLatitude=&txtLongitude=&source=",
-    # display: {
-    #     "width": 1280,
-    #     "height": 695
-    # },
-    # no_url_encode: true,
-    # http2: true,
+    cookie: "G_ENABLED_IDPS=google; hubspotutk=cfb4162b3aeaf6be0439ff8164d9474b; _ga=GA1.2.484422288.1648474523; UqZBpD3n3iPIDwJU=v1bFlJg8achqM; s_cc=true; s_nr=1651160038894; s_vnum=1653752038895&vn=1; s_invisit=true; s_sq=[[B]]; __hstc=32730849.cfb4162b3aeaf6be0439ff8164d9474b.1647879910303.1649771875857.1651160042618.42; __hssrc=1; __hssc=32730849.1.1651160042618",
     vars: { 
-        page_number: 1
+        page_num: 1
     },
+    body: URI.encode_www_form(body),
     driver: { 
         "name": "scraping manpower_can",
         "stealth": true,  
